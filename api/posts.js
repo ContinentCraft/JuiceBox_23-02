3a8632e58c63629ad11d9cd54ca2875909d7cffc
timeeatsflesh <Robert@Roberts-MacBook.local>
2023-03-06 13:28:39 -0700

    part 3

--- api/posts.js
+++ api/posts.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const postRouter = express.Router();
+const { requireUser } = require('/utils')
 
 
 const { getAllPosts } = require('../db')
+
+postRouter.post('/', requireUser, async (req, res, next) => {
+    res.send({ message: 'under construction' });
+})
 
 postRouter.get('/', async (req, res) => {
     const posts = await getAllPosts();
