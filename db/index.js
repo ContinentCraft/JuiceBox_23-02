cb3d91b399ff2831525bef80dc031e5557cdde15
timeeatsflesh <Robert@Roberts-MacBook.local>
2023-03-08 09:00:52 -0700

    deploy

--- db/index.js
+++ db/index.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const { Client } = require('pg')
 
-const client = new Client('postgres://localhost:5432/juicebox')
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev'
+  });
 
 async function createUser({ username, password, name, location }) {
     try {
